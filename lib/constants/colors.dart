fc3bd3aba92c64885b4f86f20e9625aca8e98e11
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-18 23:23:37 +0100

    Added new color

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+//import 'package:flutter/mat';
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color tdRed = Colors.red;
+const Color tdBlue = Color(0xFF5F52EE);
+
+const Color tdBGColor = Color(0xFFEEEFF5);

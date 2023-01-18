6d99586e1a94cfae35155cd6d02313838d47aaa3
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-19 00:09:51 +0100

    searchBox to widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,35 +13,39 @@ class Home extends StatelessWidget {
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
           children: [
-            Container(
-              padding:  EdgeInsets.symmetric(horizontal: 15),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(0),
-                  prefixIcon: Icon(
-                      Icons.search,
-                      color: Colors.black,
-                      size: 20
-                  ),
-                  prefixIconConstraints: BoxConstraints(
-                      maxHeight: 20,
-                      minWidth: 25
-                  ),
-                  border: InputBorder.none,
-                  hintText: 'Search',
-                  hintStyle: TextStyle(color: Colors.grey)
-                ),
-              ),
-            )
+            searchBox(),
           ],
         ),
       ),
     );
     //throw UnimplementedError();
+  }
+
+  Widget searchBox(){
+    return Container(
+      padding:  EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+                Icons.search,
+                color: Colors.black,
+                size: 20
+            ),
+            prefixIconConstraints: BoxConstraints(
+                maxHeight: 20,
+                minWidth: 25
+            ),
+            border: InputBorder.none,
+            hintText: 'Search',
+            hintStyle: TextStyle(color: Colors.grey)
+        ),
+      ),
+    );
   }
 
   AppBar _buildAppBar() {

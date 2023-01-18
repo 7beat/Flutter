9ae2174aba95239783abb169bee89a1526bd373b
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-19 00:21:23 +0100

    Added ListView

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,10 +10,29 @@ class Home extends StatelessWidget {
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15),
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 15,
+        ),
         child: Column(
           children: [
             searchBox(),
+            Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'All ToDos',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+            )
           ],
         ),
       ),
@@ -21,29 +40,19 @@ class Home extends StatelessWidget {
     //throw UnimplementedError();
   }
 
-  Widget searchBox(){
+  Widget searchBox() {
     return Container(
-      padding:  EdgeInsets.symmetric(horizontal: 15),
+      padding: EdgeInsets.symmetric(horizontal: 15),
       decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20)
-      ),
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
       child: TextField(
         decoration: InputDecoration(
             contentPadding: EdgeInsets.all(0),
-            prefixIcon: Icon(
-                Icons.search,
-                color: Colors.black,
-                size: 20
-            ),
-            prefixIconConstraints: BoxConstraints(
-                maxHeight: 20,
-                minWidth: 25
-            ),
+            prefixIcon: Icon(Icons.search, color: Colors.black, size: 20),
+            prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
             border: InputBorder.none,
             hintText: 'Search',
-            hintStyle: TextStyle(color: Colors.grey)
-        ),
+            hintStyle: TextStyle(color: Colors.grey)),
       ),
     );
   }

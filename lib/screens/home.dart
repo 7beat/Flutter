cecccba5eb331e8954d9bb6c5cfcac0ff24f751c
Tomasz Krzemiński <7beat123@wp.pl>
2023-01-19 18:43:06 +0100

    Added todo class

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:untitled/constants/colors.dart';
 import 'package:untitled/widgets/todo_item.dart';
+import 'package:untitled/model/todo.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+ Home({Key? key}) : super(key: key);
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +34,9 @@ class Home extends StatelessWidget {
                         ),
                       ),
                     ),
-                    ToDoItem(),
-                    ToDoItem(),
+
+                    for( ToDo todoo in todosList)
+                    ToDoItem(todo: todoo,),
                   ],
                 )
             )
